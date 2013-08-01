9b0b762b9b96ff0531d9875345224eed45b87d32
Markus R <markhoernchen@gmx.de>
2013-08-01 21:03:27 +0200

    added test cases and generator code

--- json-rpc-generator.js
+++ json-rpc-generator.js
@@ -3,27 +3,8 @@ var parser = require("./json-rpc-parser");
 var Generator = function(schemeText) {
   try {
     var definitions = parser.parse(schemeText);
-    console.log(JSON.stringify(definitions, null, 2));
-    
-    var interfaceDefinitions = {};
-    var typeDefinitions = {};
-    var i;
-    
-    //check for duplicate types/interfaces
-    for(i=0; i<definitions.length; i++) {
-      var def = definitions[i];
-      var name = def.name;
-      if(def.typeKind == "interface") {
-        if(name in interfaceDefinitions)
-          throw new Error("Interface '"+name+"' already exists!");
-        interfaceDefinitions[name] = def;
-      } else {
-        if(name in typeDefinitions)
-          throw new Error("Type '"+name+"' already exists!");
-        typeDefinitions[name] = def;
-      }
-    }
-    
+    //console.log(JSON.stringify(definitions, null, 2));
+        
     //build types
     //base type: BOOLEAN, STRING, FLOAT, INTEGER, VOID, STRUCT, LIST
     var types = {
@@ -59,33 +40,98 @@ var Generator = function(schemeText) {
         }
       }
     };
-    for(var name in typeDefinitions)
-      types[name] = {}; 
     function resolveType(name) {
       if(name in types)
 			  return types[name];
 		  else
 			  throw new Error("Could not resolve type '"+name+"'.");
-    }
-    function Type(name, definition) {
-      switch(definition.typeKind) {
-        case "enum":
-          var values = definition.values;
-          this.validate = function(obj) {
-            for(var i=0; i<values.length; i++)
-              if(obj === values[i])
-                return true;
-            return false;
-          };
-          break;
-        case "alias":
-          break;
-        case "struct":
-          break;
+    } 
+    for(var i=0; i<definitions.length; i++) {
+      var def = definitions[i];
+      var name = def.name;
+      if(def.typeKind != "interface") {
+        if(name in types)
+          throw new Error("Type '"+name+"' already exists!");
+        switch(def.typeKind) {
+          case "enum":
+            types[name] = {
+              baseType: "STRING",
+              validate: (function(values) {
+                return function(obj) {
+                  if(typeof(obj) !== "string")
+                    return false;
+                  for(var j=0; j<values.length; j++)
+                    if(obj === values[j])
+                      return true;
+                  return false;
+                };
+              })(def.values)
+            };
+            break;
+          case "alias":
+            if(def.isList) {
+              var elementType = resolveType(def.elementType);
+              types[name] = {
+                baseType: "LIST",
+                validate: (function(elemType) {
+                  return function(obj) {
+                    if(typeof(obj) !== "object" || !("length" in obj))
+                      return false;
+                    for(var j=0; j<obj.length; j++)
+                      if(!elemType.validate(obj[j]))
+                        return false;
+                    return true;
+                  };
+                })(elementType)
+              };
+            } else {
+              var base = resolveType(def.alias);
+              var baseType = base.baseType;
+              var validate = base.validate;
+              if(def.restriction) {
+                var restriction = def.restriction;
+                switch(restriction.restrictionKind) {
+                  case "regex":
+                    if(baseType !== "STRING")
+                      throw new Error("Regular expression restriction holds only for string types (type: "+name+").");
+                    validate = (function(vali, regex) {
+                      return function(obj) {
+                        return vali(obj) && obj.match(regex) != null;
+                      };
+                    })(validate, eval(restriction.regex));
+                    break;
+                  case "range":
+                    switch(baseType) {
+                      case "INTEGER":
+                      case "FLOAT":
+                        if(restriction.from.valueKind !== "number"
+                           || restriction.to.valueKind !== "number")
+                           throw new Error("Range restriction must consist of numeric limits (type: "+name+").");
+                        if(restriction.from.value > restriction.to.value)
+                          throw new Error("Lower limit must be smaller or equal to the upper limit (type: "+name+").");
+                        validate = (function(vali, lower, upper) {
+                          return function(obj) {
+                            return vali(obj) && obj >= lower && obj <= upper;
+                          };
+                        })(validate, restriction.from.value, restriction.to.value);
+                        break;
+                      default:
+                        throw new Error("Range restriction holds only for numeric types (type: "+name+").");
+                    }
+                    break;
+                }
+              }
+              types[name] = {
+                baseType: baseType,
+                validate: validate
+              };   
+            }
+            break;
+          case "struct":
+            break;
+        }
       }
-    }  
-    for(var name in typeDefinitions)
-      types[name].__proto__ = new Type(name, typeDefinitions[name]);  
+    }
       
     //result
     this.Types = types;
--- test.js
+++ test.js
@@ -7,6 +7,33 @@ fs.readFile("big-canvas.types", "utf8", function (err,data) {
   }
   try {
     var generator = new Generator(data);
+    
+    var testCases = [
+      { type: "ActionType", object: "BRUSH", result: true },
+      { type: "ActionType", object: "UNDO", result: false },
+      { type: "BigInteger", object: "1234", result: true },
+      { type: "BigInteger", object: "-56789", result: true },
+      { type: "BigInteger", object: "5abc", result: false },
+      { type: "BigInteger", object: "+13579", result: true },
+      { type: "StrokeWidth", object: 1, result: true },
+      { type: "StrokeWidth", object: 100, result: true },
+      { type: "StrokeWidth", object: 101, result: false },
+      { type: "StrokeWidth", object: 50, result: true },
+      { type: "StrokeWidth", object: "70", result: false },
+      { type: "ActionIds", object: ["70", "-100", "50"], result: true },
+      { type: "ActionIds", object: ["70", 50, "+abc"], result: false },
+    ];
+    
+    for(var i=0; i<testCases.length; i++) {
+      var type = testCases[i].type;
+      var object = testCases[i].object;
+      var result = generator.Types[type].validate(object);
+      var answer = result ? "Yes" : "No";
+      var passed = result == testCases[i].result ? "Passed" : "FAILED";
+      console.log("Is "+JSON.stringify(object)+" a '"+type+"'? "
+                  +answer+". "+passed+".");
+    }
+    
     //generator.ServerStub
     //generator.ClientStub
   } catch(ex) {
